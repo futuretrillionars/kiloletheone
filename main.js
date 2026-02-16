f9cef881fad2827a195cce68162e4eb876cb0bff
futuretrillionars <bedtimestory745@gmail.com>
2026-02-16 19:42:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Hero Slider
+let slides = document.querySelectorAll('.hero-slider .slide');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}, 5000);
